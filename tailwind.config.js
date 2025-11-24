beb34993f7085043758bf97eef02b413942afe4b
toha245 <mdabdulawal2001@gmail.com>
2025-11-25 01:06:10 +0600

    added a landing page

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(130.70deg, rgba(55, 82, 253, 1), rgba(107.6, 127.04, 250.2, 1) 53%, rgba(155, 168, 247.67, 1) 100%)',
+      },
+    },
+  },
+}
